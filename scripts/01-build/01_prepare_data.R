c419eaea0cbd4628fa92d254fcace9624463c031
Gustavo A. Castillo A <120594773+guscastilloa@users.noreply.github.com>
2024-05-10 09:05:39 -0500

    Change

--- scripts/01-build/01_prepare_data.R
+++ scripts/01-build/01_prepare_data.R
@@ -25,7 +25,7 @@ db <- rbind(train, validation)
 
 # 3. Feature engineering -----
 # 3.1. Construir variables a partir de descripción -----
-## número de pisos
+## Verificar variable property_type con descripción de cada observación
 clean_txt <- function(str){
  str_squish(str_to_lower(
    stringi::stri_trans_general(
@@ -42,23 +42,63 @@ db <- db %>%
            grepl("\\bapto|apartamento", description_c)==T~"Apartamento"),
          property_type2= if_else(property_type2=="Ambos",
                                  true = property_type,
-                                 false = property_type2)
-         )
+                                 false = property_type2),
+         property_type_discrepancy=if_else(property_type!=property_type2,T,F))
 table(db$property_type, db$property_type2, dnn = c("Orig", "New"), useNA = 'always')
-# Hay 991 viviendas () que con coincide el tipo de propiedad
+# Hay 536 viviendas que discrepan en el tipo de propiedad entre property_type
+# y la variable construida property_type2.
 
 db %>% 
   filter(
-    # property_type!=property_type2
-    property_type2=="Ambos"
-    # is.na(property_type2)
+    property_type!=property_type2
     ) %>% 
-  select(property_id,title,description_c,property_type,property_type2) %>% 
+  select(property_id,title,description_c,property_type,property_type2, surface_total, surface_covered, is_train) %>% 
   View()
 
+## Construir variable de número de pisos
 
-# 0923bcd0568d89de2cc71a1f
 
+
+db <- db %>%
+  mutate(n_pisos= str_extract(description, "(\\w+|\\d+) pisos")) %>%
+  mutate(n_pisos= ifelse(property_type_2=="Casa", n_pisos, NA)) 
+numeros_escritos <- c( "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez")
+numeros_numericos <- as.character(2:10)
+
+
+db <- db %>%
+  mutate(n_pisos = str_replace_all(n_pisos, setNames(numeros_numericos,numeros_escritos)))
+
+db <- db %>%
+  mutate(n_pisos_numerico = as.integer(str_extract(n_pisos, "\\d+")))  %>%
+  mutate(n_pisos_numerico = if_else(is.na(n_pisos_numerico), 1, n_pisos_numerico)) %>%
+  mutate(n_pisos_numerico = if_else(n_pisos_numerico>10, 1, n_pisos_numerico)) ### quedarnos casas de hasta 10 pisos.
+
+## número del piso del apartamento
+
+db <- db %>%
+  mutate(piso_info= str_extract(description, "(\\w+|\\d+) piso (\\w+|\\d+)"))
+
+
+numeros_escritos <- c("uno|primero|primer", "dos|segundo|segund", "tres|tercero|tercer", "cuatro|cuarto", "cinco|quinto", "seis|sexto", "siete|septimo", "ocho|octavo", "nueve|noveno", "diez|decimo|dei")
+numeros_numericos <- as.character(1:10)
+
+db <- db %>%
+  mutate(piso_info = str_replace_all(piso_info, setNames(numeros_numericos,numeros_escritos)))
+
+db <- db %>%
+  mutate(piso_numerico = as.integer(str_extract(piso_info, "\\d+")))
+
+db <- db %>%
+  mutate(piso_numerico = ifelse(piso_numerico > 20, NA, piso_numerico)) %>%
+  mutate(piso_numerico = ifelse(property_type_2=="Casa", 1, piso_numerico))
+a##########################################
+db <- db %>%
+  mutate(piso_numerico = replace_na(piso_numerico, 1))
+ggplot(db %>% filter(piso_numerico>1), aes(x = factor(piso_numerico))) +
+  geom_bar() +
+  labs(title = "", x = "Pisos", y = "Obs")+
+  theme_minimal()
 
 
 
