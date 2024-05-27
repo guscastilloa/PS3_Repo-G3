23a92d3ff27308e1fcc48987ee4d8a366878f19f
Gustavo A. Castillo A. <120594773+guscastilloa@users.noreply.github.com>
2024-05-26 21:52:39 -0500

    fix data leakage for mean and median NA imputation
    still pending to fix data leakage for kNN imputation

--- scripts/01-build/03_missing_values.R
+++ scripts/01-build/03_missing_values.R
@@ -17,7 +17,8 @@ here()
 
 # 2. Load data ----
 datos<-read_parquet("stores/db2.parquet")
-
+datos_train <- datos %>% filter(is_train==T)
+datos_test <- datos %>% filter(is_train==F)
 
 #Revisar los valores faltantes. 
 db_miss <- skimr::skim(datos) %>% select( skim_variable, n_missing)
@@ -26,39 +27,46 @@ Nobs
 db_miss<- db_miss %>% mutate(p_missing= n_missing/Nobs)
 head(db_miss)
 
-#Imputación manual. Lo voy a hacer con el promedio. 
+# Imputar datos con el promedio
 datos <- datos %>% 
   mutate(rooms_mean = 
-           if_else(is.na(rooms), mean(rooms, na.rm = T), rooms),
+           if_else(is.na(rooms), 
+                   mean(datos$rooms[datos$is_train == 1], na.rm = TRUE), rooms),
          surface_covered_mean = 
-           if_else(is.na(surface_covered), mean(surface_covered, na.rm =T),
+           if_else(is.na(surface_covered), 
+                   mean(datos$surface_covered[datos$is_train == 1], na.rm = TRUE),
                    surface_covered),
          bathrooms_mean = 
-           if_else(is.na(bathrooms), mean(bathrooms, na.rm = T),
+           if_else(is.na(bathrooms), 
+                   mean(datos$bathrooms[datos$is_train == 1], na.rm = TRUE),
                    bathrooms),
          surface_total_mean = 
-           if_else(is.na(surface_total), mean(surface_total, na.rm = T),
+           if_else(is.na(surface_total), 
+                   mean(datos$surface_total[datos$is_train == 1], na.rm = TRUE),
                    surface_total))
 
 # Imputar con la mediana
 datos <- datos %>% 
-  mutate(rooms_median = if_else(is.na(rooms), median(rooms, na.rm = T), rooms),
+  mutate(rooms_median = 
+           if_else(is.na(rooms), 
+                   median(datos$rooms[datos$is_train == 1], na.rm = TRUE),
+                   rooms),
          surface_covered_median = 
-           if_else(is.na(surface_covered), median(surface_covered, na.rm =T),
+           if_else(is.na(surface_covered), 
+                   median(datos$surface_covered[datos$is_train == 1], na.rm = TRUE),
                    surface_covered),
          bathrooms_median = 
-           if_else(is.na(bathrooms), median(bathrooms, na.rm = T),
+           if_else(is.na(bathrooms), 
+                   median(datos$bathrooms[datos$is_train == 1], na.rm = TRUE),
                    bathrooms),
          surface_total_median = 
-           if_else(is.na(surface_total), median(surface_total, na.rm = T),
+           if_else(is.na(surface_total), 
+                   mean(datos$surface_total[datos$is_train == 1], na.rm = TRUE),
                    surface_total))
 
 #Imputar datos: KNN (esto se me demora mucho. Lo descarto por el momento para empezar ya 
 #a correr modelos.)
 datos <-  kNN(datos, variable = c("bathrooms"), k = 6)
-# datos$bathrooms <- round(datos$bathrooms,0)
-# summary(datos$bathrooms)
-
 datos <-  kNN(datos, variable = c("surface_covered"), k = 6)
 datos <-  kNN(datos, variable = c("surface_total"), k = 6)
 datos <-  kNN(datos, variable = c("rooms"), k = 6)
